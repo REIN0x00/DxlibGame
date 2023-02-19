b5d76e5a7f2a7d2a2bf4570a11ba23cdd44ee7b3
S67 <81880212+hollow321@users.noreply.github.com>
2023-02-19 20:53:42 +0900

    Add Setting Full Screen

--- DxlibGame/OWindow.cpp
+++ DxlibGame/OWindow.cpp
@@ -2,6 +2,7 @@
 #include "DxLib.h"
 
 struct WINDOW_SETTINGS {
+	bool IsFullScreen = false;
 	int ScreenSize_X;
 	int Screen_Size_Y;
 	int ColorbitDepth;
@@ -10,11 +11,13 @@ struct WINDOW_SETTINGS {
 WINDOW_SETTINGS WS;
 
 OWindow::OWindow(int ScreenSize_X, int Screen_Size_Y, int ColorbitDepth) {
+	WS.IsFullScreen = true;
 	WS.ScreenSize_X = ScreenSize_X;
 	WS.Screen_Size_Y = Screen_Size_Y;
 	WS.ColorbitDepth = ColorbitDepth;
 };
 
 void OWindow::MakeWindow() {
+	ChangeWindowMode(WS.IsFullScreen);
 	SetGraphMode(WS.ScreenSize_X, WS.Screen_Size_Y, WS.ColorbitDepth);
 }
